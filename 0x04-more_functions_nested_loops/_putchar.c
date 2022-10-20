787200b95ad5bf75219bf6bc72388f0cc8b13306
highb33kay <hiibeekayvibe@gmail.com>
2022-10-20 15:58:30 -0700

    first

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stand out
+ * @c: the character to print
+ * Rreturn: On Success 1 else -1
+ */
+int _putchar(char c)
+{
+	return (write (i, &c, 1));
+}
