985475546053c1659e150b621994d3befa18f2c8
highb33kay <hiibeekayvibe@gmail.com>
2023-10-31 21:32:40 +0100

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
 /**
  * create_array - creates an array of chars.
@@ -27,4 +28,25 @@ char *create_array(unsigned int size, char c)
 	}
 
 	return (array);
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *a;
+
+	a = create_array(98, 'H');
+	if (a == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	create_array(0, 'H');
+	printf("I think it worked :)\n");
+	free(a);
+	return (0);
 }
