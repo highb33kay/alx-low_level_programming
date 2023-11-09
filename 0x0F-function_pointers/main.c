83cc1e1a86f177eb399949149e2a86fcf78cb021
highb33kay <hiibeekayvibe@gmail.com>
2023-11-09 03:32:47 +0100

    betty

--- 0x0F-function_pointers/main.c
+++ 0x0F-function_pointers/main.c
@@ -1,6 +1,19 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+
+/**
+ * print_name_wrapper - Prints a name.
+ * @name: Name to print.
+ *
+ * Return: Nothing.
+ */
+
+void print_name_wrapper(char *name)
+{
+    printf("%s\n", name);
+}
+
 /**
  * main - Entry point.
  * Test the code.
@@ -10,4 +23,10 @@
 
 int main(void)
 {
+    char *name = "Danton Rodriguez Molina";
+    void (*f)(char *) = print_name_wrapper;
+
+    print_name(name, f);
+
+    return (0);
 }
