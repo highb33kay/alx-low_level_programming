b79693b213c7096596f3c0513e025b0f84695f54
highb33kay <hiibeekayvibe@gmail.com>
2023-11-09 03:50:55 +0100

    fix syntaxc

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -8,7 +8,11 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
-    if (name && f)
+    if (name == NULL && f == NULL)
+    {
+        return;
+    }
+    else
     {
         f(name, "/n");
     }
