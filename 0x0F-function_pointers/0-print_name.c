c1a75b5de131074e356f18ed6650855e87149ce3
highb33kay <hiibeekayvibe@gmail.com>
2023-11-09 03:26:31 +0100

    print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_name - Prints a name.
+ * @name: Name to print.
+ * @f: Function pointer.
+ *
+ * Return: Nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+    if (name && f)
+    {
+        f(name);
+    }
+}
