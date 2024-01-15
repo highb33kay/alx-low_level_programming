2f24790ff7e2518c23a3a84f18b1d16fbd4eff6f
highb33kay <hiibeekayvibe@gmail.com>
2024-01-15 21:15:15 +0100

    Update random number generation in script

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo '#include <stdio.h>' > /tmp/hack.c
-echo 'int rand(void) { static int called = 0; if (called == 0) { called = 1; return 9; } else { return rand(); } }' >> /tmp/hack.c
+echo '#include <stdlib.h>' > /tmp/hack.c
+echo 'int rand(void) { return 9; }' >> /tmp/hack.c
 gcc -shared -fPIC -o /tmp/hack.so /tmp/hack.c
-LD_PRELOAD=/tmp/hack.so ./gm 9 8 10 24 75 9
+env LD_PRELOAD=/tmp/hack.so ./gm 9 8 10 24 75 9
