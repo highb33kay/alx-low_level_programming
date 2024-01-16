96583748cc39d2ade9567d11eeaf03e89e21d3da
highb33kay <hiibeekayvibe@gmail.com>
2024-01-16 08:24:38 +0100

    Fix LD_PRELOAD issue in 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-echo 'int main() { return 9; }' > libwin.c && gcc -o libwin.so -shared libwin.c && export LD_PRELOAD=$PWD/libwin.so
+echo 'int main() { return 9; }' > libwin.c
+gcc -o libwin.so -shared libwin.c
+export LD_PRELOAD=$PWD/libwin.so
